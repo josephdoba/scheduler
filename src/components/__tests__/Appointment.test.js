b79d78464b791fac40ecccf90797033b4c00d2c3
Joseph Doba <josephdoba@gmail.com>
2022-08-12 13:26:59 -0700

    Create first test batch and verify with jest

---
+++ src/components/__tests__/Appointment.test.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+
+// We import the component that we are testing
+import Application from "components/Application";
+
+afterEach(cleanup);
+
+describe("Appointment", () => {
+  it("renders without crashing", () => {
+    render(<Application />);
+  });
+  it("renders without crashing", () => {
+    console.log("Great Success 1")
+  });
+  it("renders without crashing", () => {
+    console.log("Great Success 2")
+  });
+
+// xit is used to skip tests:
+xit("Does something its suppose to do", () => {
+  // some code
+});
+
+// . skip also works:
+
+test.skip("something else its suppose to do", () => {
+  // some code
+})
+
+});
+
